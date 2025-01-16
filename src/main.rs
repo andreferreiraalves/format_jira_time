ddf28893c2678cae0fe925c68edae9de979d1664
André Ferreira <andre.ferreira@mercatustecnologia.com.br>
2025-01-16 09:37:03 -0300

    trocando senha do servidor

--- src/main.rs
+++ src/main.rs
@@ -12,28 +12,30 @@ fn main() {
     param1 = param1.to_uppercase();
     let splited = param1.split(" ");
 
-    let mut valor_total: i32 = 0;
+    let mut valor_total: f64 = 0.0;
 
     for split_value in splited {
         valor_total += get_valor("W", split_value);
         valor_total += get_valor("D", split_value);
         valor_total += get_valor("H", split_value);
+        valor_total += get_valor("M", split_value);
     }
 
     println!("Total de horas: {}h", valor_total);
 }
 
-fn get_valor(tipo: &str, split_value: &str) -> i32 {
+fn get_valor(tipo: &str, split_value: &str) -> f64 {
     if split_value.contains(tipo) {
         let valor_limpo = split_value.replace(tipo, "");
 
         return match tipo {
-            "W" => valor_limpo.parse::<i32>().unwrap() * 40,
-            "D" => valor_limpo.parse::<i32>().unwrap() * 8,
-            "H" => valor_limpo.parse::<i32>().unwrap(),
-            _ => 0,
+            "W" => valor_limpo.parse::<f64>().unwrap() * 40.0,
+            "D" => valor_limpo.parse::<f64>().unwrap() * 8.0,
+            "M" => valor_limpo.parse::<f64>().unwrap() / 60.0,
+            "H" => valor_limpo.parse::<f64>().unwrap(),
+            _ => 0.0,
         };
     }
 
-    0
+    0.0
 }
